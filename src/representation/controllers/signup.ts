9a5d22f6b0f057b45d7351ab89071396370f18a9
AmavelAntonio <amavelfranciscotomas@gmail.com>
2022-10-21 12:05:46 +0100

    apply: aplicando o helper in signup.ts

--- src/representation/controllers/signup.ts
+++ src/representation/controllers/signup.ts
@@ -1,26 +1,19 @@
 import { HttpResponse, HttpRequest } from "../protocolos/http"
 import { MissingParamsError } from "../error/missing-param"
+import { badRequest } from "../helper/http-helper"
+
 export class SignUpController {
     handle(httpRequest: HttpRequest): HttpResponse {
         if(!httpRequest.body.name){
-            return {
-                statusCode: 400, 
-                body: new MissingParamsError('name')
-            }
+            return badRequest(new MissingParamsError('name'))
         }    
 
         if(!httpRequest.body.email){
-            return {
-                statusCode: 400, 
-                body: new MissingParamsError('email')
-            }
+            return badRequest(new MissingParamsError('email'))
         }
 
         if(!httpRequest.body.confirmPassword){
-            return{
-                statusCode: 400, 
-                body: new MissingParamsError('confirmPassword')
-            }
+            return badRequest(new MissingParamsError('confirmPassword'))
         }
     }
 }
