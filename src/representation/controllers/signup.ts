89bfa5f20b67f9a81ef4f55f65e3947c79ab4d96
AmavelAntonio <amavelfranciscotomas@gmail.com>
2022-10-21 13:57:16 +0100

    ensure: SignUpController.Handle refactoring with less line code

--- src/representation/controllers/signup.ts
+++ src/representation/controllers/signup.ts
@@ -4,6 +4,16 @@ import { badRequest } from "../helper/http-helper"
 
 export class SignUpController {
     handle(httpRequest: HttpRequest): HttpResponse {
+      
+        const RequireField = ['name', 'email', 'confirmPassword']
+        for(const field of RequireField){
+            if(!httpRequest.body[field]) {
+                return badRequest(new MissingParamsError(field))
+            }
+        }
+    }
+
+     /*
         if(!httpRequest.body.name){
             return badRequest(new MissingParamsError('name'))
         }    
@@ -15,5 +25,7 @@ export class SignUpController {
         if(!httpRequest.body.confirmPassword){
             return badRequest(new MissingParamsError('confirmPassword'))
         }
-    }
+       */
+
+    
 }
