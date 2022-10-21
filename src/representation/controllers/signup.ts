2ba42dab7b4eb420c5d545a4584108e672f6deba
AmavelAntonio <amavelfranciscotomas@gmail.com>
2022-10-21 14:03:42 +0100

    ensure: apply interface in our SignUpController

--- src/representation/controllers/signup.ts
+++ src/representation/controllers/signup.ts
@@ -1,10 +1,10 @@
 import { HttpResponse, HttpRequest } from "../protocolos/http"
 import { MissingParamsError } from "../error/missing-param"
 import { badRequest } from "../helper/http-helper"
+import { Controller } from "../protocolos/controller"
 
-export class SignUpController {
+export class SignUpController implements Controller {
     handle(httpRequest: HttpRequest): HttpResponse {
-      
         const RequireField = ['name', 'email', 'confirmPassword']
         for(const field of RequireField){
             if(!httpRequest.body[field]) {
