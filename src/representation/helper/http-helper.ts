79ed171c346cd6b789911817acc7079d49b2e655
AmavelAntonio <amavelfranciscotomas@gmail.com>
2022-10-25 11:28:56 +0100

    feat: refactor signUpController with new serverError class()

--- src/representation/helper/http-helper.ts
+++ src/representation/helper/http-helper.ts
@@ -1,6 +1,11 @@
 import { HttpResponse } from "../protocolos/http";
-
+import { ServerError } from "../error/server-error";
 export const badRequest = (error: Error): HttpResponse => ({
     statusCode:400, 
     body: error
+})
+
+export const serverError = () => ({
+    statusCode: 500, 
+    body: new ServerError()
 })
