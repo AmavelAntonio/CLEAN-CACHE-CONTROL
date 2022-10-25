8eaaf458a30d1ad1a63d01f2648f4a87734bfc88
AmavelAntonio <amavelfranciscotomas@gmail.com>
2022-10-25 11:18:09 +0100

    feat: ensure signUpController returns 500 if EmailValidator throws

--- src/representation/controllers/signup.ts
+++ src/representation/controllers/signup.ts
@@ -4,6 +4,7 @@ import { badRequest } from "../helper/http-helper"
 import { Controller } from "../protocolos/controller"
 import { EmailValidator } from "../protocolos/email-validator"
 import { InvalidEmailError } from "../error/InvalidEmail-param"
+import { ServerError } from "../error/server-error"
 
 export class SignUpController implements Controller {
     private readonly emailValidator: EmailValidator; 
@@ -13,7 +14,8 @@ export class SignUpController implements Controller {
     }
     
     handle(httpRequest: HttpRequest): HttpResponse {
-        const RequireField = ['name', 'email', 'confirmPassword']
+        try{
+            const RequireField = ['name', 'email', 'confirmPassword']
         for(const field of RequireField){
             if(!httpRequest.body[field]) {
                 return badRequest(new MissingParamsError(field))
@@ -24,6 +26,12 @@ export class SignUpController implements Controller {
         
         if(!isValid){
             return badRequest(new InvalidEmailError('email'))
+        }
+        }catch(error){
+            return{
+                statusCode: 500,
+                body: new ServerError()
+            }
         }
     } 
 }
